6edaf7ddaff11d7a6087ee28489887c589536d13
JLE <league@wts-imac-11.attlocal.net>
2018-11-19 13:22:40 -0800

    workshop

--- src/section2/FourSquare.java
+++ src/section2/FourSquare.java
@@ -6,27 +6,39 @@ import org.jointheleague.graphical.robot.Robot;
 public class FourSquare {
 	
 	// 2. Create a new Robot
+		Robot walle = new Robot();
 
 	void go() {
 		// 4. Make the robot move as fast as possible
-
+walle.setSpeed(1000);
 		// 5. Set the pen width to 5
-
+walle.setPenWidth(5);
 		// 6. Use a for loop to repeat steps #7 to #8, four times...
-
+for (int i = 0; i < 4; i++) {
+	walle.setRandomPenColor();
+	walle.turn(90);
+}
 			// 7. Set the pen color to random
-	
+	walle.setRandomPenColor();
 			// 1. Call the drawSquare() method
+		drawSquare();
 	
 			// 8. Turn the robot 90 degrees to the right
-
+walle.turn(90);
 	}
 
 	
 	void drawSquare() {
-		JOptionPane.showMessageDialog(null, "yay! you called the drawSquare() method!");
+ 
 		/* 3. Fill in the code to draw a square inside the method below. */
+		walle.penDown();
+		for (int i = 0; i < 4; i++) {
+			walle.move(100);
+			walle.turn(90);	
+		}
 		
+		
+
 		
 	}
 
--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -7,8 +7,23 @@ public class MyFirstJavaProgram {
 	
 	public static void main(String[] args) {
 		
-		// START HERE
 	
+Robot walle = new Robot();
+walle.move(100);
+walle.turn(90);
+walle.sparkle();
+walle.penDown();
+walle.move(90);
+walle.turn(90);
+walle.move(90);
+walle.turn(90);
+walle.move(90);
+walle.turn(90);
+walle.move(90);
+walle.penUp();
+
+
+
 		
 		
 	}
